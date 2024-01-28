1ec66e85fb0554a7aac5c4b3ac6f35d8fd38b28e
ryanlerler <lyp2726@outlook.com>
2024-01-28 16:20:53 +0800

    Set up MySQL, bcrypt and user model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,6 +14,11 @@ class UserController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required', 'min:8', 'max:200'],
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        User::create($incomingFields);
+
         return 'Hello from our controller';
     }
 }
